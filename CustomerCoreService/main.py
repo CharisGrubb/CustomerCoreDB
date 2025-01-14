799e2bb053740cc5be36eafd51738406e7b48e74
Charis Grubb <charisgrubb@gmail.com>
2025-01-14 15:09:57 -0500

    Testing opentelemetry potential

--- CustomerCoreService/main.py
+++ CustomerCoreService/main.py
@@ -1,9 +1,17 @@
+from Endpoints import Customers, Telemetry, Users
 from fastapi import FastAPI 
+from opentelemetry import sdk 
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
 
-from .Endpoints import Customers, Telemetry, Users
+
+
+
+sdk.trace.set_trace_profider(sdk.trace.TraceProvider(resource = sdk.resources.Resource.create({sdk.resources.SERVICE_NAME:"CustomerCore"})))
+trace_provider = sdk.trace.get_tracer_provider()
 
 
 app = FastAPI() 
+FastAPIInstrumentor.instrument_app(app)
 
 app.include_router(Customers.router)
 app.include_router(Telemetry.router)
