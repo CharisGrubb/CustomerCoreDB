7f5f88ef314fe1dd6b0b59eb554c7089953debea
Charis Grubb <charisgrubb@gmail.com>
2025-02-02 14:35:34 -0500

    Added middleware CORS and dynamic load for menu. One html file for all pages

--- CustomerCoreService/main.py
+++ CustomerCoreService/main.py
@@ -1,5 +1,6 @@
 from Endpoints import Customers, Users, Sales
 from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
@@ -16,6 +17,16 @@ trace_provider = trace.get_tracer_provider()
 
 app = FastAPI() 
 FastAPIInstrumentor.instrument_app(app)
+
+origins = ["http://localhost"
+           , "http://localhost:8000"
+           ,"https://cdn-script.com/ajax/libs/jquery/3.7.1/jquery.js"]
+
+app.add_middleware(CORSMiddleware
+                   , allow_origins = origins
+                   , allow_credentials=True
+                   ,allow_methods=["*"] #allow GET, POST, PUT, etc
+                   ,allow_headers=["*"]) #FILTER METHODS AND HEADERS LATER AFTER FULLY FUNCTIONAL
 
 app.include_router(Customers.router)
 app.include_router(Users.router)
@@ -35,3 +46,15 @@ async def read_item(request: Request):
     return templates.TemplateResponse(
         request=request, name="index.html", context={"id": id}
     )
+
+@app.get("/login", response_class=HTMLResponse)
+async def log_in(request: Request):
+    print('Inside html call')
+    return templates.TemplateResponse(
+        request=request, name="login.html")
+
+@app.get("/menu", response_class=HTMLResponse)
+async def load_menu(request: Request):
+    print('Inside menu call')
+    return templates.TemplateResponse(
+        request=request, name="menu.html")
