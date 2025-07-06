d657d6a42c9dbae17bd8c4487c28735525c122d0
Charis <charisgrubb@gmail.com>
2025-07-06 11:01:30 -0400

    Building out more model

--- CustomerCoreService/DB/DB_Models.py
+++ CustomerCoreService/DB/DB_Models.py
@@ -20,6 +20,7 @@ class Users(Base):
     last_name: Mapped[str] 
     user_role: Mapped[str]
     create_date: Mapped[datetime] = mapped_column(default=lambda: datetime.now())
+    modify_date: Mapped[datetime] = mapped_column(onupdate=lambda: datetime.now())
 
     @property 
     def fullname(self):
@@ -35,13 +36,28 @@ class Sessions(Base):
 class Customer(Base):
     __tablename__ = "customers"
     id: Mapped[int] = mapped_column(primary_key=True)
+    first_name: Mapped[str]
+    middle_name: Mapped[Optional[str]]
+    last_name: Mapped[str]
+    source: Mapped[str] #Source from where customer came, another software or could be web/in store/phone
+    create_date: Mapped[datetime] = mapped_column(default=lambda: datetime.now())
+    modify_date: Mapped[datetime] = mapped_column(onupdate=lambda: datetime.now())
+
 
 #To map user to system to sale for reporting and aggregation
 class Sales(Base):
     __tablename__ = "sales"
     id: Mapped[int] = mapped_column(primary_key=True)
 
+
 #For system logging and view
 class Log(Base):
     __tablename__ = 'logs'
+
     id: Mapped[int] = mapped_column(primary_key=True)
+    log_name: Mapped[str]
+    log_type: Mapped[str]
+    log_source: Mapped[str]
+    log_level: Mapped[str]
+    log_message: Mapped[str]
+    log_datetime: Mapped[datetime] = mapped_column(default=lambda: datetime.now())
